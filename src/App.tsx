3c1aab7209256186619d23a2df10a15b58b1dda6
sujithbogituser <sujithkumar.pv@blueowl.com>
2026-03-23 19:51:18 +0530

    Add App.tsx component displaying title 'Hello World 1'

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World 1</h1>
+            <ul>
+                {/* Items will be displayed here */}
+            </ul>
+        </div>
+    );
+};
+
+export default App;
